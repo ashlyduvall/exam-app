0d9d53584e4e0cfa937948853418031784eb1e30
Ashly Duvall <ashly@workstation.vm.duvall.org.uk>
2022-01-15 10:44:28 +0000

    Properly rotating the js file hash

--- .docker/build.sh
+++ .docker/build.sh
@@ -2,4 +2,14 @@
 
 NGINX_ROOT="/usr/share/nginx/html"
 
-echo build build build
+set -o xtrace
+
+cd $NGINX_ROOT
+
+cat ./js/app/*.js > /tmp/combined.js
+HASH=$(md5sum /tmp/combined.js | cut -c1-8)
+JS_FILE="main.${HASH}.js"
+
+mv /tmp/combined.js "${NGINX_ROOT}/js/${JS_FILE}"
+# sed -i "s:main.js:${JS_FILE}:g" *.html
+sed -i -E "s:main.([a-f0-9]{8}).js:${JS_FILE}:g" *.html
