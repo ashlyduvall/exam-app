e9f3accea460044030174ceefa2c0c0d3c00ffd3
Ashly Duvall <ashly@workstation.vm.duvall.org.uk>
2022-05-28 15:11:35 +0100

    Table colour should be better now

--- web/js/app/2_helpers.js
+++ web/js/app/2_helpers.js
@@ -6,8 +6,35 @@
 main.filter('exam_markdown', ['$sce', '$sanitize', function($sce, $sanitize) {
     return function (text) {
         text = text || '';
-        text = text.replace(/\n/g, '<br/>');
-        text = $sanitize(text);
+
+        // Check for any tables
+        text = text.split("\n");
+
+        let in_table = false;
+        for (let line_index in text) {
+            let this_line = text[line_index];
+
+            // In table mode
+            if (this_line[0] == '|') {
+                this_line = '<tr>' + this_line + '</td></tr>';
+                this_line = this_line.replace(/\|/g, '</td><td>');
+                this_line = this_line.replace('<tr></td>', '<tr>');
+                if (!in_table) {
+                    in_table = true;
+                    this_line = '<table class="table" style="color: lightblue;">' + this_line;
+                }
+            } else if(in_table) {
+                in_table = false;
+                this_line = '</table><br />' + this_line;
+            } else {
+                this_line += '<br />'
+            }
+
+            text[line_index] = this_line;
+        }
+        text = text.join('');
+
+        // text = $sanitize(text);
         return $sce.trustAsHtml(text);
     };
 }]);
